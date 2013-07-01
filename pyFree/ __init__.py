166fbd0012593b61eb8668a8045206d3d84daea3
cart0uche <hafid.saou@gmail.com>
2013-07-01 13:28:29 +0200

    Update  __init__.py

--- pyFree/ __init__.py
+++ pyFree/ __init__.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
  
+"""
+    Unofficial API that implements access to Freebox Server "Révolution" (http://dev.freebox.fr/sdk/os/)
+"""
+ 
+__version__ = "0.0.1"
